1a09c76eb4b8ab7be78ea5b578fcc0588771e439
ecastrejon@ucol.mx <ecastrejon@ucol.mx>
2023-05-29 19:51:19 -0600

    Agregar archivos de proyecto.

---
+++ Shared/Modelo/Prestamos.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libreria.Shared.Modelo
+{
+    public class Prestamos
+    {
+        public int Id { get; set; }
+
+        public int ClienteId { get; set; }
+
+        public virtual Cliente? Cliente { get; set; }
+
+        public int LibroId { get; set; }
+
+        public virtual Libro? Libro { get; set; }
+
+        [Required(ErrorMessage = "Favor de escribir la fecha de inicio del prestamo"), MaxLength(10)]
+        public DateTime? Inicioprestamo { get; set; }
+
+        [Required(ErrorMessage = "Favor de escribir la fecha de vencimiento del prestamo"), MaxLength(10)]
+        public DateTime? Vencimientoprestamo { get; set; }
+
+        public DateTime? Fechadevolucion { get; set; }
+
+
+
+    }
+}
